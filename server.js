28427a86e7dd311331ad15909f509ee1c094184f
Manjaro <aria.groult@outlook.fr>
2020-06-12 10:01:36 +0200

    Update beautified code

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require("express");
 const routes = require("./router_components/endpoints.js");
 const userRoutes = require("./router_components/userEndpoints.js");
 const jwt = require("jsonwebtoken");
-var cors = require('cors');
+const cors = require('cors');
+
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,10 +16,9 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 
-
 app.use('/', routes)
 app.use('/user', userRoutes)
 
-app.listen(process.env.PORT || 3001, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port || 3001, function(){
+    console.log("Express server listening on port %d in %s mode", port, app.settings.env);
   });
